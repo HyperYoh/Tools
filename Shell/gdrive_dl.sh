03439f93babb6691320fffc08bcdbdfee2694e4a
HyperYoh <hyper1yoh@gmail.com>
2023-07-19 07:08:06 +0200

    Update gdrive_dl.sh: replace tab by space

--- Shell/gdrive_dl.sh
+++ Shell/gdrive_dl.sh
@@ -5,7 +5,7 @@ gdrive_dl()
     if (( "$#" != 2 )) || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
         echo Usage: $0 FILEID FILENAME
     else
-	wget "drive.google.com/u/3/uc?id=$1&export=download&confirm=yes" -O "$2"
+        wget "drive.google.com/u/3/uc?id=$1&export=download&confirm=yes" -O "$2"
     fi
 }
 
